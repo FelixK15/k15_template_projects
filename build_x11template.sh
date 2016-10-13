b7c26947a9004dc7cb20565fee952795dd35d4b3
Felix Klinge <f.klinge15@gmail.com>
2016-10-13 21:34:06 +0200

    Update build_x11template.sh
    normal x11 template project does need to link against openGL

--- build_x11template.sh
+++ build_x11template.sh
@@ -1,3 +1,3 @@
 C_FILE_TO_COMPILE="k15_x11template.c"
-GCC_OPTIONS="-ansi -std=c99 -g3 -L/usr/X11/lib -lX11 -lGL"
+GCC_OPTIONS="-ansi -std=c99 -g3 -L/usr/X11/lib -lX11"
 gcc $C_FILE_TO_COMPILE $GCC_OPTIONS
