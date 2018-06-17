2abd1ffb2336ca8ab5fdd1aa78403156dfe54d14
Felix <f.klinge15@gmail.com>
2018-06-18 00:47:08 +0200

    fixed build scripts to work with vs 2017

--- build_win32template_opengl_intermediate.bat
+++ build_win32template_opengl_intermediate.bat
@@ -16,17 +16,15 @@ set VS_PATH=
 ::check whether this is 64bit windows or not
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
 
-IF %OS%==64BIT set REG_FOLDER=HKLM\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\
-IF %OS%==32BIT set REG_FOLDER=HKLM\SOFTWARE\Microsoft\VisualStudio\
+IF %OS%==64BIT set REG_FOLDER=HKLM\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7
+IF %OS%==32BIT set REG_FOLDER=HKLM\SOFTWARE\Microsoft\VisualStudio\SxS\VS7
 
 ::Go to end if nothing was found
 IF %REG_FOLDER%=="" GOTO DECISION
 
 ::try to get get visual studio path from registry for different versions
-FOR /l %%G IN (14, -1, 8) DO (
-	set REG_PATH=%REG_FOLDER%%%G.0
-	set REG_COMMAND=reg query !REG_PATH! /v InstallDir
-
+FOR /l %%G IN (20, -1, 8) DO (
+	set REG_COMMAND=reg query !REG_FOLDER! /v %%G.0
 	!REG_COMMAND! >nul 2>nul
 
 	::if errorlevel is 0, we found a valid installDir
@@ -35,7 +33,7 @@ FOR /l %%G IN (14, -1, 8) DO (
 		FOR /F "skip=2 tokens=*" %%A IN ('!REG_COMMAND!') DO (
 			set VS_PATH=%%A
 			::truncate stuff we don't want from the output
-			set VS_PATH=!VS_PATH:~24!
+			set VS_PATH=!VS_PATH:~18!
 			set FOUND_PATH=1
 			goto DECISION
 		)
@@ -48,9 +46,17 @@ IF !FOUND_PATH!==0 (
 	echo Could not find valid Visual Studio installation.
 ) ELSE (
 	echo Starting build process...
-	set VCVARS_PATH="!VS_PATH!..\..\VC\vcvarsall.bat"
-	set CL_PATH="!VS_PATH!..\..\VC\bin\cl.exe"
+	set VCVARS_PATH="!VS_PATH!VC\vcvarsall.bat"
+
+	call !VCVARS_PATH! >nul 2>nul
+
+	if !errorlevel! neq 0 (
+		set VCVARS_PATH="!VS_PATH!VC\Auxiliary\Build\vcvarsall.bat"
+		call !VCVARS_PATH! x86 >nul 2>nul
+	)
+
+	set CL_PATH="!VS_PATH!cl.exe"
+	set CL_OPTIONS=/nologo /Fe!EXECUTABLE_NAME! /MD /TC /W3 /Od /Zi
 	set BUILD_COMMAND=!CL_PATH! !C_FILE_NAME! !CL_OPTIONS!
-	call !VCVARS_PATH!
 	call !BUILD_COMMAND!
 ) 
